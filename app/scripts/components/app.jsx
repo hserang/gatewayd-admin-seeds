4c487b89faeb1a66ef46a76309ebec60ed25f5b1
Rod Calumpong <rod@ripple.com>
2014-10-20 17:56:21 -0700

    [TASK] Create routes

--- app/scripts/components/app.jsx
+++ app/scripts/components/app.jsx
@@ -1,9 +1,15 @@
+"use strict";
+
 var React = require('react');
 var TopBar = require('./header/top-bar.jsx');
 var Footer = require('./footer/footer.jsx');
-var Hellos = require('./hellos/hello.jsx');
 
-var model = new (require('./../stores/hello-model'))();
+var Router = require('react-router');
+var Link = Router.Link;
+
+var Navbar = require('react-bootstrap').Navbar;
+var NavItem = require('react-bootstrap').NavItem;
+var Nav = require('react-bootstrap').Nav;
 
 require('react-bootstrap');
 
@@ -12,12 +18,21 @@ var App =
 
   render:function(){
     return (
-      <div>
-        <TopBar />
-        <div className="container">
-          <Hellos model={model} />
-          <Footer />
-        </div>
+      <div className="container">
+        <Navbar>
+          <div className="navbar-header">
+            <a className="navbar-brand">
+              We have routing!
+            </a>
+          </div>
+          <Nav>
+            <NavItem><Link to="hellos">Fancy</Link></NavItem>
+            <NavItem><Link to="hellos2">Slang</Link></NavItem>
+            <NavItem><Link to="hellos3">Gorilla</Link></NavItem>
+          </Nav>
+        </Navbar>
+        <this.props.activeRouteHandler />
+        <Footer />
       </div>
     )
   }
--- app/scripts/main.jsx
+++ app/scripts/main.jsx
@@ -2,9 +2,37 @@
 
 var React = require('react');
 
+var Router = require('react-router');
+var Route = Router.Route;
+var Routes = Router.Routes;
+var DefaultRoute = Router.DefaultRoute;
+// var NotFoundRoute = Router.NotFoundRoute;
+
+var Hellos = require('./components/hellos/hello.jsx');
+var HellosHellos = require('./components/hellos/hello2.jsx');;
+var HellosHellosHellos = require('./components/hellos/hello3.jsx');
+
+var Model = require('./stores/hello-model');
+
+var model = new Model();
+var modelModel = new Model();
+var modelModelModel = new Model();
+
 // needed for dev tools to work
 window.React = React;
 
 var App = require('./components/app.jsx');
 
-React.renderComponent(<App />, document.getElementById('content-main'));
+var routes = (
+  <Routes location="history">
+    <Route name="app" path="/" handler={App}>
+      <Route name="hellos" handler={Hellos} model={model} />
+      <Route name="hellos2" handler={HellosHellos} model={modelModel} />
+      <Route name="hellos3" handler={HellosHellosHellos} model={modelModelModel} />
+      <DefaultRoute handler={Hellos} model={model} />
+    </Route>
+  </Routes>
+);
+
+// React.renderComponent(<App />, document.getElementById('content-main'));
+React.renderComponent(routes, document.getElementById('content-main'));
