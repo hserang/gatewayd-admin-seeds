d39cb083a67e96cb779409281ca4382d61e538e2
Rod Calumpong <Rod.Calumpong@gmail.com>
2014-11-03 15:22:04 -0800

    [TASK] Set up login session restoration

--- app/scripts/components/app.jsx
+++ app/scripts/components/app.jsx
@@ -1,25 +1,44 @@
 "use strict";
 
 var React = require('react');
+
+var Navigation = require('react-router').Navigation;
+var session = require('../session/models/session');
+
 var RouterHeader = require('./header/router-header.jsx');
 var Footer = require('./footer/footer.jsx');
 
-var session = require('../session/models/session');
-
 require('react-bootstrap');
+
+var restoreSession = function() {
+  var oldSession = sessionStorage.length === 0 ? null : sessionStorage.getItem('session');
+
+  if (oldSession) {
+    session.set(JSON.parse(oldSession));
+  }
+
+  if (session.isLoggedIn()) {
+    this.transitionTo('payments');
+  } else {
+    this.transitionTo('/');
+  }
+};
 
 var App =
   React.createClass({
+    mixins: [Navigation],
 
-  render:function(){
-    return (
-      <div className="container">
-        <RouterHeader />
-        {this.props.activeRouteHandler()}
-        <Footer />
-      </div>
-    )
-  }
+    render:function(){
+      // restoreSession.call(this);
+
+      return (
+        <div className="container">
+          <RouterHeader />
+          {this.props.activeRouteHandler()}
+          <Footer />
+        </div>
+      )
+    }
 });
 
 module.exports = App;
