5a8dbc4326dd9412b3e7a3d71619472755abf96f
Rod Calumpong <Rod.Calumpong@gmail.com>
2014-11-14 17:20:16 -0800

    [TASK] Continuously fetch ripple transactions from database

--- app/scripts/components/router.jsx
+++ app/scripts/components/router.jsx
@@ -13,6 +13,30 @@ var Payments = require('../modules/payments/components/payments.jsx');
 var LoginForm = require('../modules/session/components/login-form.jsx');
 var Session = require('../modules/session/components/session.jsx');
 
+var session = require('../modules/session/models/session');
+
+// continuously fetch when tab is active
+var paymentActions = require('../modules/payments/actions.js');
+var heartbeats = require('heartbeats');
+var pollingHeart = new heartbeats.Heart(5000);
+
+var pollWhenActive = function() {
+  if (session.isLoggedIn()) {
+    paymentActions.fetchRippleTransactions();
+  }
+};
+
+pollingHeart.onBeat(1, pollWhenActive);
+
+window.onfocus = function() {
+  pollingHeart.clearEvents();
+  pollingHeart.onBeat(1, pollWhenActive);
+};
+
+window.onblur = function() {
+  pollingHeart.clearEvents();
+};
+
 // needed for dev tools to work
 window.React = React;
 
