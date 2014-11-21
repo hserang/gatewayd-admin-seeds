a17bf951d8c50199adba8d4e4cc784eacc9d1a23
Rod Calumpong <Rod.Calumpong@gmail.com>
2014-11-21 15:05:50 -0800

    [TASK] Add payments/payment-create submodule for sending ripple payments

---
+++ app/scripts/modules/payments/components/payment-create.jsx
@@ -0,0 +1,258 @@
+'use strict';
+
+var _ = require('lodash');
+var React = require('react');
+var Row = require('react-bootstrap').Row;
+var Col = require('react-bootstrap').Col;
+var Label = require('react-bootstrap').Label;
+var Input = require('react-bootstrap').Input;
+var Button = require('react-bootstrap').Button;
+var ProgressBar = require('react-bootstrap').ProgressBar;
+var LoadingBar = require('../../../shared/components/loading-bar/components/loading-bar.jsx');
+var loadingBarActions = require('../../../shared/components/loading-bar/actions');
+var paymentActions = require('../actions');
+
+var PaymentCreate = React.createClass({
+  validationMap: {
+    valid: 'success',
+    invalid: 'warning'
+  },
+
+  typeMap: {
+    address: 'string',
+    amount: 'number',
+    currency: 'string',
+    destinationTag: 'number',
+    sourceTag: 'number',
+    invoiceId: 'string',
+    memo: 'string'
+  },
+
+  formatInput: function(rawInputRef, type) {
+    var formattedInput = rawInputRef.getValue().trim();
+
+    if (!formattedInput) {
+      return null;
+    }
+
+    return type === 'number' ? formattedInput * 1 : formattedInput;
+  },
+
+  showFieldValidationResult: function(isValid, fieldName, errorMessage) {
+    if (isValid) {
+      var validField = {};
+
+      validField[fieldName] = {isValid: 'valid'};
+
+      this.setState(validField);
+    } else {
+      var invalidField = {};
+
+      invalidField[fieldName] = {
+        isValid: 'invalid',
+        errorMessage: errorMessage
+      };
+
+      this.setState(invalidField);
+    }
+
+    if (fieldName === 'address') {
+      this.setState({
+        disableAddressField: false,
+        disableSubmitButton: false
+      });
+    }
+  },
+
+  validateField: function(fieldName, shouldForceValidate) {
+    var fieldValue = this.formatInput(this.refs[fieldName], this.typeMap[fieldName]);
+    var clearFieldValidation = {};
+    clearFieldValidation[fieldName] = {};
+
+    this.setState(clearFieldValidation);
+
+    if (fieldValue !== null || shouldForceValidate) {
+      this.props.model.validateField(fieldName, fieldValue);
+    }
+  },
+
+  validateAddress: function(shouldForceValidate) {
+    var addressFieldValue = this.formatInput(this.refs.address, this.typeMap.address);
+
+    this.setState({
+      address: {}
+    });
+
+    if (addressFieldValue !== null || shouldForceValidate) {
+      this.setState({
+        disableAddressField: true,
+        disableSubmitButton: true
+      });
+
+      this.props.model.validateAddress(addressFieldValue);
+    }
+  },
+
+  handleSubmit: function(e) {
+    e.preventDefault();
+
+    var _this = this;
+    var payment = {
+      address: this.formatInput(this.refs.address, this.typeMap.address),
+      amount: this.formatInput(this.refs.amount, this.typeMap.amount),
+      currency: this.formatInput(this.refs.currency, this.typeMap.currency),
+      destinationTag: this.formatInput(this.refs.destinationTag, this.typeMap.destinationTag),
+      sourceTag: this.formatInput(this.refs.sourceTag, this.typeMap.sourceTag),
+      invoiceId: this.formatInput(this.refs.invoiceId, this.typeMap.invoiceId),
+      memo: this.formatInput(this.refs.memo, this.typeMap.memo)
+    };
+
+    this.setState({
+      disableForm: true,
+      submitButtonLabel: 'Sending Payment...',
+    });
+
+    // force validate all fields, even if null
+    this.validateAddress(true);
+    this.validateField('amount', true);
+    this.validateField('currency', true);
+    this.validateField('destinationTag', true);
+    this.validateField('sourceTag', true);
+    this.validateField('invoiceId', true);
+    this.validateField('memo', true);
+
+    paymentActions.sendPaymentAttempt(payment);
+  },
+
+  handleClose: function() {
+    this.props.onSubmitSuccess();
+  },
+
+  handleError: function() {
+    this.setState({
+      disableForm: false,
+      submitButtonLabel: 'Re-Submit Payment?',
+    });
+  },
+
+  dispatchSendPaymentComplete: function(model, data) {
+    this.handleClose();
+
+    paymentActions.sendPaymentComplete(data.payment);
+  },
+
+  handlePolling: function() {
+    this.advanceProgressBar(25);
+  },
+
+  getInitialState: function() {
+    return {
+      address: {},
+      amount: {},
+      currency: {},
+      destinationTag: {},
+      sourceTag: {},
+      invoiceId: {},
+      memo: {},
+      disableForm: false,
+      submitButtonLabel: 'Submit Payment',
+    };
+  },
+
+
+  componentDidMount: function() {
+    this.props.model.on('validationComplete', this.showFieldValidationResult);
+    this.props.model.on('sync', this.dispatchSendPaymentComplete);
+    this.props.model.on('error invalid', this.handleError);
+  },
+
+  componentWillUnmount: function() {
+    this.props.model.off('validationComplete');
+    this.props.model.off('sync');
+    this.props.model.off('error');
+    this.props.model.off('invalid');
+  },
+
+  render: function() {
+    var graphLink = (
+      <a href={this.state.graphUrl}>
+        See this transaction in action: Ripple Graph
+      </a>
+    );
+    var progressBar = (
+      <div>
+        <LoadingBar />
+        {this.state.showGraphLink ? graphLink : null}
+      </div>
+    );
+
+    return (
+      <div>
+        <h2>Send Payment</h2>
+        <form onSubmit={this.handleSubmit}>
+
+          <Input type="text" ref="address"
+            label={<div><Label bsStyle="info">Required</Label> Destination Address: </div>}
+            bsStyle={this.validationMap[this.state.address.isValid]}
+            disabled={this.state.disableForm || this.state.disableAddressField}
+            autoFocus={true} onBlur={this.validateAddress.bind(this, false)} />
+          <Label bsStyle="warning">{this.state.address.errorMessage}</Label>
+          <Row>
+            <Col xs={6}>
+
+              <Input type="tel" ref="amount"
+                label={<div><Label bsStyle="info">Required</Label> Amount: </div>}
+                bsStyle={this.validationMap[this.state.amount.isValid]}
+                disabled={this.state.disableForm} onBlur={this.validateField.bind(this, 'amount', false)} />
+              <Label bsStyle="warning">{this.state.amount.errorMessage}</Label>
+            </Col>
+            <Col xs={6}>
+              <Input type="text" ref="currency"
+                label={<div><Label bsStyle="info">Required</Label> Currency: </div>}
+                bsStyle={this.validationMap[this.state.currency.isValid]}
+                disabled={this.state.disableForm} onBlur={this.validateField.bind(this, 'currency', false)} />
+              <Label bsStyle="warning">{this.state.currency.errorMessage}</Label>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={6}>
+              <Input type="tel" ref="destinationTag"
+                label="Destination Tag:"
+                bsStyle={this.validationMap[this.state.destinationTag.isValid]}
+                disabled={this.state.disableForm} onBlur={this.validateField.bind(this, 'destinationTag', false)} />
+              <Label bsStyle="warning">{this.state.destinationTag.errorMessage}</Label>
+            </Col>
+            <Col xs={6}>
+              <Input type="tel" ref="sourceTag"
+                label="Source Tag:"
+                bsStyle={this.validationMap[this.state.sourceTag.isValid]}
+                disabled={this.state.disableForm} onBlur={this.validateField.bind(this, 'sourceTag', false)} />
+              <Label bsStyle="warning">{this.state.sourceTag.errorMessage}</Label>
+            </Col>
+          </Row>
+          <Input type="text" ref="invoiceId"
+            label="Invoice Id:"
+            bsStyle={this.validationMap[this.state.invoiceId.isValid]}
+            disabled={this.state.disableForm} onBlur={this.validateField.bind(this, 'invoiceId', false)} />
+          <Label bsStyle="warning">{this.state.invoiceId.errorMessage}</Label>
+          <Input type="textarea" ref="memo"
+            label="Memo:"
+            bsStyle={this.validationMap[this.state.memo.isValid]}
+            disabled={this.state.disableForm} onBlur={this.validateField.bind(this, 'memo', false)} />
+          <Label bsStyle="warning">{this.state.memo.errorMessage}</Label>
+          <Button className="pull-right" bsStyle="primary" bsSize="large" type="submit"
+            disabled={this.state.disableForm || this.state.disableSubmitButton}
+            block>
+            {this.state.submitButtonLabel}
+          </Button>
+          <br />
+          <br />
+          <br />
+          {this.state.showProgressBar ? progressBar : null}
+        </form>
+      </div>
+    );
+  }
+});
+
+module.exports = PaymentCreate;
---
+++ app/scripts/modules/payments/models/payment-create.js
@@ -0,0 +1,236 @@
+'use strict';
+
+var _ = require('lodash');
+var $ = require('jquery');
+var Backbone = require('backbone');
+var RippleName = require('ripple-name');
+var dispatcher = require('../../../dispatchers/admin-dispatcher');
+var session = require('../../session/models/session');
+var paymentActions = require('../config.json').actions;
+var appConfig = require('../../../shared/app-config');
+
+Backbone.$ = $;
+
+var Payment = Backbone.Model.extend({
+  defaults: {
+    address: '',
+    amount: 0,
+    currency: '',
+    destinationTag: 0,
+    sourceTag: 0,
+    invoiceId: '',
+    memo: ''
+  },
+
+  requiredAttrs: {
+    address: {
+      type: 'string',
+      minLength: 1, // some min for ripple address, but none for ripple name
+      isRequired: true
+    },
+    amount: {
+      type: 'number', // decimal,
+      isRequired: true
+    },
+    currency: {
+      type: 'string',
+      minLength: 1,
+      isRequired: true
+    }
+  },
+
+  validationRules: {
+    address: {
+      type: 'string',
+      minLength: 1, // some min for ripple address, but none for ripple name
+      isRequired: true
+    },
+    amount: {
+      type: 'number', // decimal,
+      isRequired: true
+    },
+    currency: {
+      type: 'string',
+      minLength: 1,
+      isRequired: true
+    },
+    destinationTag: {
+      type: 'number',
+      isRequired: false
+    },
+    sourceTag: {
+      type: 'number',
+      isRequired: false
+    },
+    invoiceId: {
+      type: 'string',
+      minLength: 1,
+      isRequired: false
+    },
+    memo: {
+      type: 'string',
+      minLength: 1,
+      isRequired: false
+    }
+  },
+
+  url: appConfig.baseUrl,
+
+  initialize: function() {
+    _.bindAll(this);
+
+    dispatcher.register(this.dispatchCallback);
+  },
+
+  dispatchCallback: function(payload) {
+    var handleAction = {};
+
+    handleAction[paymentActions.sendPaymentAttempt] = this.sendPaymentAttempt;
+
+    if (!_.isUndefined(handleAction[payload.actionType])) {
+      handleAction[payload.actionType](payload.data);
+    }
+  },
+
+  updateBaseUrl: function(newBaseUrl) {
+    this.url = newBaseUrl;
+  },
+
+  validationErrors: [],
+
+  handleObject: function(value, minLength) {
+    if (value === null) {
+      return false;
+    }
+
+    if (Array.isArray(value)) {
+      return value.length >= minLength;
+    }
+
+    return Object.keys(value).length >= minLength;
+  },
+
+  handleString: function(value, minLength) {
+    return !!value && value.length >= minLength;
+  },
+
+  testValid: function(value, attr, rules) {
+    if (value === null && !rules[attr].isRequired) {
+      return true;
+    }
+
+    var testValid = {
+      object: this.handleObject,
+      string: this.handleString,
+    };
+    var isDefined = !_.isUndefined(value);
+    var type = rules[attr].type === 'array' ? 'object' : rules[attr].type;
+    var isValid = typeof value === 'number' ? !isNaN(value) : typeof value === type;
+
+    if (isValid && !_.isUndefined(testValid[typeof value])) {
+      isValid = testValid[typeof value](value, rules[attr].minLength);
+    }
+
+    // custom error messaging
+    if (!isDefined) {
+      this.validationErrors.push(attr + ' is undefined');
+    } else if (!isValid) {
+      this.validationErrors.push(attr + ' is invalid');
+    }
+
+    return isDefined && isValid;
+  },
+
+  validate: function(attributes) {
+    var _this = this;
+
+    this.validationErrors = [];
+
+    var isValid = _.reduce(attributes, function(accumulator, value, attr) {
+      if (_.isUndefined(_this.validationRules[attr])) {
+        return accumulator && true;
+      }
+
+      if (_this.testValid(value, attr, _this.validationRules)) {
+        return accumulator && true;
+      } else {
+        return false;
+      }
+    }, true);
+
+    if (!Object.keys(attributes).length) {
+      isValid = false;
+    }
+
+    if (!isValid) {
+      return this.validationErrors.join(', ');
+    }
+  },
+
+  isValid: function() {
+    this.validate(this.attributes);
+
+    return !this.validationError;
+  },
+
+  postPayment: function() {
+    var _this = this;
+
+    this.save(null, {
+      url: session.get('gatewaydUrl') + '/v1/payments/outgoing',
+      contentType: 'application/json',
+      headers: {
+        Authorization: session.get('credentials')
+      }
+    });
+  },
+
+  sendPaymentAttempt: function(payment) {
+    var _this = this;
+
+    this.validateAddress(payment.address);
+
+    if (this.isValid()) {
+      this.postPayment();
+    }
+  },
+
+  handleFieldValidation: function(validationResult, fieldRef) {
+    if (!_.isUndefined(validationResult)) {
+      this.trigger('validationComplete', false, fieldRef, validationResult);
+    } else {
+      this.trigger('validationComplete', true, fieldRef, '');
+    }
+  },
+
+  validateAddress: function(address) {
+    var _this = this;
+    RippleName.lookup(address)
+    .then(function(data) {
+      if (data.exists) {
+        var addressAttr = {
+          address: data.address
+        };
+
+        _this.set(addressAttr);
+        _this.handleFieldValidation(_this.validate(addressAttr), 'address');
+      } else {
+        _this.trigger('validationComplete', false, 'address', 'ripple name/address does not exist');
+      }
+    })
+    .error(function() {
+      _this.trigger('validationComplete', false, 'address', 'ripple name lookup failed');
+    });
+  },
+
+  validateField: function(fieldName, fieldValue) {
+    var updatedField = {};
+    updatedField[fieldName] = fieldValue;
+
+    this.set(updatedField);
+
+    this.handleFieldValidation(this.validate(updatedField), fieldName);
+  }
+});
+
+module.exports = Payment;
