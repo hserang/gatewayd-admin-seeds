1bd41dea571b32f462a57403881b6e264bc446f0
Rod Calumpong <Rod.Calumpong@gmail.com>
2014-11-21 15:17:51 -0800

    [TASK] Add app component for application initialization

--- app/scripts/components/app.jsx
+++ app/scripts/components/app.jsx
@@ -1,26 +1,53 @@
-var React = require('react');
-var TopBar = require('./header/top-bar.jsx');
-var Footer = require('./footer/footer.jsx');
-var Hellos = require('./hellos/hello.jsx');
+"use strict";
 
-var model = new (require('./../stores/hello-model'))();
+var React = require('react');
+var Navigation = require('react-router').Navigation;
+var DocumentTitle = require('react-document-title');
+
+var session = require('../modules/session/models/session');
+var sessionActions = require('../modules/session/actions');
+var RouterHeader = require('./header/top-bar.jsx');
+var Greeting = require('../shared/components/greeting/greeting.jsx');
 
 require('react-bootstrap');
 
+var topBarConfig = {
+  brandName: 'Gatewayd Basic',
+  wrapperClass: 'navbar-inverse navbar-fixed-top top-bar container-fluid',
+  links: [ ]
+};
+
 var App =
   React.createClass({
+    mixins: [Navigation],
 
-  render:function(){
-    return (
-      <div>
-        <TopBar />
-        <div className="container">
-          <Hellos model={model} />
-          <Footer />
+    redirectToLogin: function() {
+      if (!session.isLoggedIn()) {
+        this.transitionTo('/login');
+      }
+    },
+
+    render:function(){
+      if (!session.get('lastLogin')) {
+        sessionActions.restore();
+        this.redirectToLogin();
+      }
+
+      return (
+        <div>
+          <RouterHeader setup={topBarConfig} />
+          <div className="container">
+            <div className="row">
+              <div className="col-sm-12  col-md-12 main">
+              <DocumentTitle title='Gatewayd Basic Admin'>
+                {this.props.activeRouteHandler()}
+              </DocumentTitle>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-    )
-  }
+      )
+    }
 });
 
 module.exports = App;
