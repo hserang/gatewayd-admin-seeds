4a6548696d564914fa1153efc9ef055550745fc7
Haroun Serang <haroun@ripple.com>
2014-11-26 15:01:28 -0800

    [FEATURE] Create Form Input Component

---
+++ app/scripts/shared/components/form/input.jsx
@@ -0,0 +1,50 @@
+'use strict';
+
+var _ = require('lodash');
+var React = require('react');
+var Formsy = require('formsy-react');
+
+var Input = React.createClass({
+  mixins: [Formsy.Mixin],
+
+  getDefaultProps: function() {
+    return {
+      inputClass: "form-control"
+    };
+  },
+
+  changeValue: function (event) {
+    this.setValue(event.currentTarget.value);
+  },
+
+  render: function() {
+    var className = 'form-group '
+    var errorMessage = this.getErrorMessage();
+    var label;
+
+    if (this.props.label) {
+      label = <label htmlFor={this.props.name}> {this.props.label} </label>;
+    } else {
+      label = null;
+    }
+
+    className += this.showRequired() ? 'required' : this.showError() ? 'error' : '';
+
+    return (
+      <div className={className}>
+        {label}
+        <input
+          className={this.props.inputClass}
+          type={this.props.type}
+          disabled={this.props.disabled}
+          onChange={this.changeValue}
+          value={this.getValue()}
+          id={this.props.name}
+        />
+        <span>{errorMessage}</span>
+      </div>
+    );
+  }
+});
+
+module.exports = Input;
