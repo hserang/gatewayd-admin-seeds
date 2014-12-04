6c344587a0dce5b7e0ba4038da9dc749f8b14041
Haroun Serang <haroun@ripple.com>
2014-12-03 18:05:15 -0800

    [TASK] Update and Cleanup Tests

--- app/scripts/modules/session/tests/fixtures/successes.js
+++ app/scripts/modules/session/tests/fixtures/successes.js
@@ -1,7 +1,14 @@
+"use strict";
+
+var Chance = require('chance');
+var chance = new Chance();
+
 var data = {
   admin: {
+    gatewaydUrl: 'http://localhost:5000',
     sessionKey: 'gatewaydapikey',
-    lastLogin: Date.now()
+    lastLogin: Date.now(),
+    credentials: chance.string({length: 64})
   }
 };
 
